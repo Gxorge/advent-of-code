62327db4ea5ee2efa30c0167c6f6d42f3b59d618
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2022-12-21 21:39:17 +0000

    aoc: updated main execution

--- src/main/kotlin/uk/hotten/adventofcode/AdventOfCode.kt
+++ src/main/kotlin/uk/hotten/adventofcode/AdventOfCode.kt
@@ -1,12 +1,19 @@
 package uk.hotten.adventofcode
 
+
 fun main() {
 
-    /**
-     * val day = DayX()
-     *
-     * day.part1()
-     * day.part2()
-     */
+    /*val day = DayX()
+
+    val time = System.currentTimeMillis()
+
+    day.part1()
+    println("Part 1 took ${System.currentTimeMillis() - time}ms to run.")
+
+    val timeP2 = System.currentTimeMillis()
+    day.part2()
+    println("Part 2 took ${System.currentTimeMillis() - timeP2}ms to run.")
+
+    println("Both parts took ${System.currentTimeMillis() - time}ms to run.")*/
     
 }
